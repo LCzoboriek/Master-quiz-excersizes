a51ddcd484f4bb6e395c2ab0ef1bb0c919969ec6
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 15:14:45 +0100

    Basic game

---
+++ dicegame.rb
@@ -0,0 +1,11 @@
+#This following program will be a game between two people where dice are thrown
+# and whoever has the highest number on their dice will win
+
+
+random_number = rand(10)
+my_number = rand(10)
+if random_number > my_number
+  return false
+else
+  return true
+end
