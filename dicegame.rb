712576df58294961d5df81ebfdcde6a4491cf7fa
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 15:22:27 +0100

    Upgraded version 2.0

--- dicegame.rb
+++ dicegame.rb
@@ -1,9 +1,9 @@
 #This following program will be a game between two people where dice are thrown
 # and whoever has the highest number on their dice will win
+# Upgrade the dice game: now both I and my opponent roll two six-sided dice each.
 
-
-random_number = rand(10)
-my_number = rand(10)
+random_number = rand(6) + rand(6)
+my_number = rand(6) + rand(6)
 if random_number > my_number
   return false
 else
