3eed25c858baa28ed3a49872a66b161d05ba4d0c
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 20:37:05 +0100

    updated user greeting

--- usergreeting.rb
+++ usergreeting.rb
@@ -2,5 +2,11 @@
 # The following conditionals will be put into replace
 # Itll ask for a name, if it begins with S itll shout at the person
 # Any other name and it'll just say Hello with their name
-puts "Please enter your name here: "
-name = gets.chomp
+
+def greeter
+  puts "Please enter your name here: "
+  name = gets.chomp
+  name.chr == "S" ? (puts name.upcase) : (puts "Hi, #{name}")
+end
+
+greeter
