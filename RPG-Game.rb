92cb0015a287f8e1e71c7e9e1438925b25261e51
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 20:45:20 +0100

    Start off with the starting direction

---
+++ RPG-Game.rb
@@ -0,0 +1,6 @@
+# This program is a game, there are different directions the player can move
+# if they go in certain directions, they die, if they go the right way, twice
+# they win
+
+
+STARTING_DIRECTION = "Forward"
