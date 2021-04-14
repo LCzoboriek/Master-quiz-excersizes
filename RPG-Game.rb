22951a494a253d5544889938a61ee38928e4ab5a
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 21:03:39 +0100

    Made the change to add the second level

--- RPG-Game.rb
+++ RPG-Game.rb
@@ -8,6 +8,27 @@ STARTING_DIRECTION = "Forward"
 puts "Hello there adventurer, welcome to the Occasim, you find yourself
  deep in a forest, there are sounds all around you, you have one direction you
  can go but depending on where you go, you could face death. Where do you
-   go?: "
+   go? (Forward, Left, Right or Back?): "
 
-direction = user.getschomp
+
+direction = gets.chomp.downcase
+  if direction == "forward"
+    puts "Good choice! You moved forward through the forest"
+      puts "What direction will you choose now?: "
+      second_direction = gets.chomp.downcase
+        if second_direction == "forward"
+          puts "Congratulations! You made it through Occasim, you get to live >:]"
+        elsif second_direction == "left"
+          puts "Oh no, you ran into a werewolf, he stares you in your eyes, and one
+          swipe and he chops you down with his claws"
+        elsif second_direction == "right"
+          puts "Oh no, you ran into a goblin, he laughs as he stabs you with the
+          sharpest knife he has"
+        end
+  elsif direction == "left"
+    puts "Oh no, you ran into a werewolf, he stares you in your eyes, and one
+    swipe and he chops you down with his claws"
+  elsif direction == "right"
+    puts "Oh no, you ran into a goblin, he laughs as he stabs you with the
+    sharpest knife he has"
+  end
