36f4c314c7caee0a6cd433813cd01723cba31b9f
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 20:49:19 +0100

    Started the adventure off, added user input

--- RPG-Game.rb
+++ RPG-Game.rb
@@ -4,3 +4,10 @@
 
 
 STARTING_DIRECTION = "Forward"
+
+puts "Hello there adventurer, welcome to the Occasim, you find yourself
+ deep in a forest, there are sounds all around you, you have one direction you
+ can go but depending on where you go, you could face death. Where do you
+   go?: "
+
+direction = user.getschomp
