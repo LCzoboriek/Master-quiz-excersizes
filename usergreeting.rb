f61aea76c04d2013d0fc647c457bed47e7b4057a
Luke Czoboriek <lc169782@protonmail.com>
2021-04-14 20:22:15 +0100

    First commit

---
+++ usergreeting.rb
@@ -0,0 +1,6 @@
+# The following program is an attempt at making a greeter
+# The following conditionals will be put into replace
+# Itll ask for a name, if it begins with S itll shout at the person
+# Any other name and it'll just say Hello with their name
+puts "Please enter your name here: "
+name = gets.chomp
